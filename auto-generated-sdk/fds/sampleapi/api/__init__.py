95057309c9463a90604fa5fa39909aa9a72608f6
afernandes85 <58183178+afernandes85@users.noreply.github.com>
2020-10-26 23:11:38 -0400

    feat(sdk): Auto-commit from 'sampleapi-sdk-generator' repository PR 1 for SDK version v0.0.0 (#1)

---
+++ auto-generated-sdk/fds/sampleapi/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from fds.sampleapi.api.pet_api import PetApi
+from fds.sampleapi.api.store_api import StoreApi
+from fds.sampleapi.api.user_api import UserApi
